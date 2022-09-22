00bdd4fb2037e174392bb9facf1e813c05435bc5
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2022-09-22 20:44:55 -0300

    Create prototipon2.cpp

---
+++ prototipon2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+	
+	char c=' ';
+	
+	
+	cout<<"\n";
+	char x;
+	x=getch();
+	switch(x){
+		
+
+		
+		case 49:
+			cout<<"um"<<endl;
+			break;
+			
+		case 50:
+			cout<<"dois"<<endl;
+			break;
+			
+		case 51:
+			cout<<"tres"<<endl;
+			break;
+			
+		case 52:
+			cout<<"quatro"<<endl;
+			break;
+	}
+	
+		
+	
+	return 0;
+}
